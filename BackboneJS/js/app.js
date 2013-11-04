c07b01c747a0588b8b0bd71cb0eab9de655cb120
twol <a157523222@gmail.com>
2013-11-04 12:12:46 +0900

    正解をコミット

--- BackboneJS/js/app.js
+++ BackboneJS/js/app.js
@@ -4,6 +4,16 @@ var Task = Backbone.Model.extend({
     defaults: {
         title: 'do something',
         completed: false
+    },
+    validate: function(attrs) {
+        if ( _.isEmpty(attrs.title) ) {
+            return 'title must not be empty';
+        }
+    },
+    initialize: function() {
+        this.on('invalid', function(model, error) {
+            $('#error').html(error);
+        })
     }
 });
 var Tasks = Backbone.Collection.extend({ model: Task });
@@ -38,12 +48,46 @@ var TaskView = Backbone.View.extend({
 });
 var TasksView = Backbone.View.extend({
     tagName: 'ul',
+    initialize: function() {
+        this.collection.on('add', this.addNew, this);
+        this.collection.on('change', this.updateCount, this);
+        this.collection.on('destroy', this.updateCount, this);
+    },
+    addNew: function(task) {
+        var taskView = new TaskView({model: task});
+        this.$el.append(taskView.render().el);
+        $('#title').val('').focus();
+        this.updateCount();
+    },
+    updateCount: function() {
+        var uncompletedTasks = this.collection.filter(function(task) {
+            return !task.get('completed');
+        });
+        $('#count').html(uncompletedTasks.length);
+    },
     render: function() {
         this.collection.each(function(task) {
             var taskView = new TaskView({model: task});
             this.$el.append(taskView.render().el);
         }, this);
+        this.updateCount();
         return this;
+    }
+});
+
+var AddTaskView = Backbone.View.extend({
+    el: '#addTask',
+    events: {
+        'submit': 'submit'
+    },
+    submit: function(e) {
+        e.preventDefault();
+        // var task = new Task({title: $('#title').val()});
+        var task = new Task();
+        if (task.set({title: $('#title').val()}, {validate: true})) {
+            this.collection.add(task);
+            $('#error').empty();
+        }
     }
 });
 
@@ -61,6 +105,8 @@ var tasks = new Tasks([
 ]);
 
 var tasksView = new TasksView({collection: tasks});
+var addTaskView = new AddTaskView({collection: tasks});
+
 $('#tasks').html(tasksView.render().el);
 
 

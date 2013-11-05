e1f99360761b4aa751ba8ceec0e72f0336ff167c
twol <a157523222@gmail.com>
2013-11-06 00:50:40 +0900

    Railsの用意　http://qiita.com/yuku_t/items/c14d4554f6af8b4e6159

---
+++ BackboneJS_rails/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BackboneJSRails::Application.config.secret_token = 'a7726cec7a8d4d8f38f0388c893fe63b4335bb179d246caba0402c365909fcf1353f82a4a9ab427d4367acafe11b309ea06eee7c5cd48929bf686ad4e951d0e1'

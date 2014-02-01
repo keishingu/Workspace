fd49ffdf9ff129903528629afcc608b54c1f2c40
twol <a157523222@gmail.com>
2014-02-01 23:48:56 +0900

    #8

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,23 +1,20 @@
-var http = require('http');
+var http = require('http'),
+	fs = require('fs');
 var settings = require('./settings');
 var msg;
 var server = http.createServer();
 
 server.on('request', function(req, res) {
-	res.writeHead(200, {'Content-Type': 'text/plain'});
-	switch (req.url) {
-		case '/about':
-			msg = "about this page";
-			break;
-		case '/profile':
-			msg = "about me";
-			break;
-		default:
-			msg = "404";
-			break;
-	}
-	res.write('hello from' + msg);
-	res.end();
+	fs.readFile(__dirname + '/public_html/hello.html', 'utf-8', function(err, data) {
+		if (err) {
+			res.writeHead(404, {'Content-Type': 'text/plain'});
+			res.write("not found");
+			return res.end();
+		}
+		res.writeHead(200, {'Content-Type': 'text/html'});
+		res.write(data);
+		return res.end();
+	});
 });
 server.listen(settings.port, settings.host);
 console.log(settings.port, settings.host);

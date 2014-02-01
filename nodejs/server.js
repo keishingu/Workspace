e39ff0ff8baef5ca612b0acd939f38df1d411f0b
twol <a157523222@gmail.com>
2014-02-01 20:04:05 +0900

    #7

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,10 +1,22 @@
 var http = require('http');
 var settings = require('./settings');
+var msg;
 var server = http.createServer();
 
 server.on('request', function(req, res) {
 	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.write('hello world');
+	switch (req.url) {
+		case '/about':
+			msg = "about this page";
+			break;
+		case '/profile':
+			msg = "about me";
+			break;
+		default:
+			msg = "404";
+			break;
+	}
+	res.write('hello from' + msg);
 	res.end();
 });
 server.listen(settings.port, settings.host);

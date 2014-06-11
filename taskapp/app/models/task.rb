c2b59c8fb06d465bfa85f7ad12041e72d3c14341
twol <a157523222@gmail.com>
2014-06-12 02:44:06 +0900

    finished dot install

---
+++ taskapp/app/models/task.rb
@@ -0,0 +1,12 @@
+class Task < ActiveRecord::Base
+  belongs_to :project
+  validates :title,
+	presence: {
+		message: "入力してください"
+	},
+	length: {
+		minimum: 3,
+		message: "短すぎ！"
+	}
+	scope :unfinished, -> { where(done: false) }
+end
